9b760f2437c88b3ca1004f377f9178568f4a4773
Chaitanya A S <aschaitanya519@gmail.com>
2023-10-09 12:00:46 +0000

    simple Maven Hello World project..

---
+++ src/main/java/com/socgen/scr/grm/App.java
@@ -0,0 +1,13 @@
+package com.socgen.scr.grm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
